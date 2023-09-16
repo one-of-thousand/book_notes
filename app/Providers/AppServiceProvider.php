54e5852e58ca1d8d410f4bfc652f663b13e898fe
one-of-thousand <128983154+one-of-thousand@users.noreply.github.com>
2023-09-16 18:09:57 +0900

    https通信の変更

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -23,7 +23,7 @@ class AppServiceProvider extends ServiceProvider
     {
         Schema::defaultStringLength(191);
         Paginator::useBootstrap();
-        if(\App::environment(['production'])){
+        if(\App::environment(['production'])|| \App::environment(['develop'])){
             \URL::forseScheme('https');
         }
     }
