d57564995b7964ec95a3e31b580d99192c0f0839
one-of-thousand <128983154+one-of-thousand@users.noreply.github.com>
2023-09-17 12:06:03 +0900

    https通信変更

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -5,6 +5,8 @@ namespace App\Providers;
 use Illuminate\Support\ServiceProvider;
 use Illuminate\Support\Facades\Schema;
 use Illuminate\Pagination\Paginator;
+use Illuminate\Support\Facades\App;
+use Illuminate\Support\Facades\URL;
 
 class AppServiceProvider extends ServiceProvider
 {
@@ -23,8 +25,8 @@ class AppServiceProvider extends ServiceProvider
     {
         Schema::defaultStringLength(191);
         Paginator::useBootstrap();
-        if(\App::environment(['production'])|| \App::environment(['develop'])){
-            \URL::forseScheme('https');
+        if(App::environment(['production'])|| App::environment(['develop'])){
+            URL::forseScheme('https');
         }
     }
 }
